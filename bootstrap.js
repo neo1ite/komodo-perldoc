649da1c2578130385be3b4607c46e9eaac408d4f
Sergey Kovalev <info@neolite.ru>
2026-08-20 22:42:00 +0300

    Update bootstrap diagnostics for 0.1.4

--- bootstrap.js
+++ bootstrap.js
@@ -1,7 +1,7 @@
 const { classes: Cc, interfaces: Ci, utils: Cu } = Components;
 Cu.import("resource://gre/modules/Services.jsm");
 
-const VERSION = "0.1.3";
+const VERSION = "0.1.4";
 const PREFIX = "[komodo-perldoc " + VERSION + "]";
 
 function debugFile() {
