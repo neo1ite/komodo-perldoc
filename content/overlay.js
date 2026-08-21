69c56d7eecaa98f5c12529c6492c54419f2816cf
Sergey Kovalev <info@neolite.ru>
2026-08-21 11:17:14 +0300

    Bump overlay version to 0.1.9

--- content/overlay.js
+++ content/overlay.js
@@ -2,7 +2,7 @@
     const { classes: Cc, interfaces: Ci, utils: Cu } = Components;
     Cu.import("resource://gre/modules/Services.jsm");
 
-    const VERSION = "0.1.8";
+    const VERSION = "0.1.9";
     const PREFIX = "[komodo-perldoc " + VERSION + "]";
     const SCOPE_DOCS_CONTRACT = "@activestate.com/commando/koScopeDocs;1";
     const RETRY_DELAY_MS = 100;
